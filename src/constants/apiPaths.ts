97e3b5aa626a5e3fb90309be2126f412ddb982e0
Jegius <jegius@gmail.com>
2020-11-21 01:22:29 +0400

    Task 6 implemented

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://ungftsxv80.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://ungftsxv80.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://ungftsxv80.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://lirnkhspqk.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://ungftsxv80.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
